37f9b7ddf0cb2f5583ac3956d53ce02a1f91bc60
AgustinSaguier <Agustinsaguier420@gmail.com>
2022-03-30 12:55:03 -0400

    name change

--- Informatica 3A/formulario_7/main.php
+++ Informatica 3A/formulario_7/main.php
@@ -5,9 +5,37 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style type="text/css">
+            
+            body{
+                background-color: #3aafa9;
+                color: #def2f1;
+            }
+    
+            h1{
+            background-color:white ;
+            color: #17252A;
+            width: 600px;
+            }
+
+            h3{
+            background-color: #def2f1;
+            color: #2b7a78;
+            width: 100px;
+            }
+
+            h4{
+                background-color: #2b7a78;
+                color: white;
+                width: 100px;
+            }
+    
+        </style>
 </head>
 <body>
     <?php
+
+        echo("<h1>SISTEMA CALCULO DEL SALARIO</h1>");    
 
         $nombre=$_REQUEST ["nombre"];
         $apellido=$_REQUEST ["apellido"];
@@ -17,16 +45,16 @@
         $pASO=$_REQUEST["pASO"];
         $hijos=$_REQUEST["hijos"];
 
-        echo ("$nombre $apellido");
+        echo ("<h2>$nombre $apellido<h2>");
 
-        echo ("<p>Salario:</p>");
+        echo ("<h3>Salario:</h3>");
 
         $salario_base=$monto*$horas;
         $salario_ips=($salario_base*$pIPS)/100;
         $salario_aso=($salario_base*$pASO)/100;
         $salario_hijos=($salario_base*($hijos*3))/100;
         $salario_final=($salario_base-$salario_ips-$salario_aso+$salario_hijos);
-        echo("$salario_final")
+        echo("<h4>$salario_final G<h4>")
     ?>
 </body>
 </html>
