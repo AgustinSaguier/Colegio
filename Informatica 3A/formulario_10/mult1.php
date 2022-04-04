f04784ef1aaefbb2e2ea4d82bfb037f3691cb1f2
AgustinSaguier <Agustinsaguier420@gmail.com>
2022-04-04 10:00:04 -0400

    name change

---
+++ Informatica 3A/formulario_10/mult1.php
@@ -0,0 +1,12 @@
+<?php
+echo("<table>");
+for ($y=1; $y<=5; $y++){
+    echo("<tr>");
+    for ($x=1; $x<=5; $x++){
+        echo("<td>hello</td>");
+    }
+    echo("<tr/>");
+}
+echo("</table>");
+
+?>
