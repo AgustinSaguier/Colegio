4691591d628fa4580fc124677e5eb5c590c9c44f
AgustinSaguier <Agustinsaguier420@gmail.com>
2022-04-05 15:44:41 -0400

    name change

--- Informatica 3A/formulario_10/mult1.php
+++ Informatica 3A/formulario_10/mult1.php
@@ -1,9 +1,16 @@
 <?php
+$contador=0;
 echo("<table>");
-for ($y=1; $y<=5; $y++){
+echo("<tr>");
+echo("<td>X</td>");
+for ($z=1; $z<=7, $z++){
+    echo("<td>$z</td>");
+}
+echo("</tr>");
+for ($y=1; $y<=7; $y++){
     echo("<tr>");
-    for ($x=1; $x<=5; $x++){
-        echo("<td>hello</td>");
+    for ($x=1; $x<=7; $x++){
+        echo("<td>".$y*$x."</td>");
     }
     echo("<tr/>");
 }
