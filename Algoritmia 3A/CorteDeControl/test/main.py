e06add35f6ea6cfdfe265d00c6db7e6e89fc3da2
AgustinSaguier <Agustinsaguier420@gmail.com>
2022-04-05 12:59:19 -0400

    name change

--- Algoritmia 3A/CorteDeControl/test/main.py
+++ Algoritmia 3A/CorteDeControl/test/main.py
@@ -1,6 +1,24 @@
+central=0
+cordillera=0
+value=0
+
 with open("main.txt") as file:
     for record in file:
+        record=record.strip()
         if record:
             values=record.split(",")
-            if values[0]=="Central":
-                central=0+int([1])
+            claveactual=values[0]
+            claveanterior=claveactual
+            if claveanterior==claveactual:
+                value=value+int(values[1])
+                print (claveactual+" "+str(value))
+                value=0
+            
+            
+            #if values[0]=="":
+            #    central=central+int(values[1])
+            #elif values[0]=="":
+            #    cordillera=cordillera+int(values[1])
+
+#print("Central: "+str(central))
+#print("Cordillera: "+str(cordillera))
