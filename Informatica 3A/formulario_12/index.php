f05621014388d387f017af6d5a833affed0c8d0d
AgustinSaguier <Agustinsaguier420@gmail.com>
2022-04-08 12:00:20 -0400

    name change

--- Informatica 3A/formulario_12/index.php
+++ Informatica 3A/formulario_12/index.php
@@ -1,14 +1,22 @@
 <?php
 $num=1;
-$cant=2;
+$tema=$_REQUEST["tema"];
+$cant=$_REQUEST["cantidad"];
 
-echo"hello";
-echo"<br>";
+if ($tema=="Autos"){ 
+    for ($x=1; $x<=$cant; $x++){
+        echo "<img src='img/car$num.jpg' width='100px' height='100px'>";
+        echo"<br>";
+        $num=$num+1;
 
-for ($x=1; $x<=$cant; $x++){
-    echo "<img src='img/car$num.jpg' width='100px' height='100px'>";
-    echo"<br>";
-    $num=$num+1;
+    }
+}
+elseif($tema=="Frutas"){
+    for ($x=1; $x<=$cant; $x++){
+        echo "<img src='img/fruta$num.jpg' width='100px' height='100px'>";
+        echo"<br>";
+        $num=$num+1;
 
- }
+    } 
+}
 ?>
