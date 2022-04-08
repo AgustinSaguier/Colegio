e1b44b2b4945b47852f80619ee78cd3c6b9bed17
AgustinSaguier <Agustinsaguier420@gmail.com>
2022-04-08 11:46:08 -0400

    name change

--- Informatica 3A/formulario_12/index.php
+++ Informatica 3A/formulario_12/index.php
@@ -1,7 +1,14 @@
 <?php
+$num=1;
+$cant=4;
 
+echo"hello";
+echo"<br>";
 
+for ($x=1; $x<=$cant; $x++){
+    echo "<img src='img/car$num.jpg' width='100px' height='100px'>";
+    echo"<br>";
+    $num=$num+1;
 
-
-
+ }
 ?>
