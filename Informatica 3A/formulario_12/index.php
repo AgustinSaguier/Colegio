4f1167e22607b271698517fcf270792032cceae0
AgustinSaguier <Agustinsaguier420@gmail.com>
2022-04-08 12:09:44 -0400

    name chagne

--- Informatica 3A/formulario_12/index.php
+++ Informatica 3A/formulario_12/index.php
@@ -3,6 +3,7 @@ $num=1;
 $tema=$_REQUEST["tema"];
 $cant=$_REQUEST["cantidad"];
 
+
 if ($tema=="Autos"){ 
     for ($x=1; $x<=$cant; $x++){
         echo "<img src='img/car$num.jpg' width='100px' height='100px'>";
