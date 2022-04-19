4f7cf029282db48019a552ff46110fe912d36a09
AgustinSaguier <Agustinsaguier420@gmail.com>
2022-04-19 12:12:58 -0400

    name change

---
+++ Algoritmia 3A/CorteDeControl/problemaPlanteado2/ventas.py
@@ -0,0 +1,48 @@
+
+claveAnterior=0
+claveActual=0
+total=0
+linea=[]
+
+def corte():
+    global claveAnterior
+    global total
+    print (claveAnterior, "Total: ", total)
+    total=0
+
+def calcula():
+    global valor
+    global claveActual
+    val=lista[-1]
+    valor=val
+    claveActual=lista[0]
+
+def splitLine():
+    global lista
+    global linea
+    linea=archivo.readline()
+    lista=linea.split(",")
+    
+    
+
+
+try:
+    archivo=open("notas.txt","r")
+    splitLine()
+
+    calcula()
+    claveAnterior=claveActual
+    while (linea!=""): 
+        #lista=lista[:-1]
+        if (claveAnterior==claveActual):
+            total=total+int(valor)         
+            splitLine()
+            calcula()
+        else:
+           corte()
+           claveAnterior=claveActual  
+    
+    corte()
+    claveAnterior=claveActual       
+except IOError:
+    print ("Error")
