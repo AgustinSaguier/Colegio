bb0233a943c18273c9a188f6f54dc4fd5818882b
AgustinSaguier <Agustinsaguier420@gmail.com>
2022-04-19 15:23:23 -0400

    name change

--- Algoritmia 3A/CorteDeControl/test/test.py
+++ Algoritmia 3A/CorteDeControl/test/test.py
@@ -1,8 +1,20 @@
+contador=0
+
+
 with open("test.txt") as file:
-    for record in file:
+        record=file.readline()
         record=record.strip()
         values=record.split(",")
         claveActual=values[0]
-        if record:
-            print("l")
-            claveAnterior=claveActual
+        claveAnterior=claveActual
+        while (record!=""):
+            if claveAnterior==claveActual:
+                contador=contador+1
+                record=file.readline()
+                record=record.strip()
+                values=record.split(",")
+                claveActual=values[0]
+            else:
+                print("La clave " + claveAnterior + " aparece "+ str(contador)+" veces")
+                contador=0
+                claveAnterior=claveActual
