1fcdc33f4138ab36052cea751ffe3ac5a55ba9db
AgustinSaguier <Agustinsaguier420@gmail.com>
2022-04-21 14:11:42 -0400

    name change

--- Algoritmia 3A/CorteDeControl/problemaPlanteado4/main.py
+++ Algoritmia 3A/CorteDeControl/problemaPlanteado4/main.py
@@ -1,19 +1,53 @@
+arr=[]
+
 
 with open("main.txt") as file:
         record=file.readline()
         record=record.strip()
-        values=record.split(",")
+        arr.append(record)
+        for record in file:
+            record=record.strip()
+            arr.append(record)
+realArr=sorted(arr)
+new=open("newMain.txt","w")
+for item in realArr:
+    new.write(item)
+    new.write("\n")
+new.close()
+
+contador=0
+sumaTotal=0
+suma=0
+
+with open("newMain.txt")as archivo ,open("resumen.txt","w") as resumen:
+        linea=archivo.readline()
+        linea=linea.strip()
+        values=linea.split(",")
+        if (values[0]=="Articulos"):
+            linea=archivo.readline()
+            linea=linea.strip()
+            values=linea.split(",")
         claveActual=values[0]
+        suma=int(values[1])
         claveAnterior=claveActual
         while (record!=""):
             if claveAnterior==claveActual:
-                record=file.readline()
-                record=record.strip()
-                values=record.split(",")
+                contador=contador+1
+                sumaTotal=sumaTotal+suma
+                linea=archivo.readline()
+                linea=linea.strip()
+                values=linea.split(",")
                 claveActual=values[0]
-                if (record == ""):
+                if (linea == ""):
                     break
+                suma=int(values[1])
             else:
-                #print("La clave " + claveAnterior + " aparece "+ str(contador)+" veces con un valor de " + str(sumaTotal))
+                print("La clave "  + claveAnterior + " aparecio " + str(contador) + " diferentes ocasiones con una suma total de " + str(sumaTotal))
+                resumen.write("La clave "  + claveAnterior + " aparecio " + str(contador) + " diferentes ocasiones con una suma total de " + str(sumaTotal)+"\n")
+                contador=0
+                sumaTotal=0
                 claveAnterior=claveActual
-#print("La clave " + claveAnterior + " aparece "+ str(contador)+" veces con un valor de " + str(sumaTotal))
+        print("La clave "  + claveAnterior + " aparecio " + str(contador) + " diferentes ocasiones con una suma total de " + str(sumaTotal))
+        resumen.write("La clave "  + claveAnterior + " aparecio " + str(contador) + " diferentes ocasiones con una suma total de " + str(sumaTotal))
+
+print(realArr)
