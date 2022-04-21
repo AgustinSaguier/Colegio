5b31a4194dc8f24c1179576761f85022d51537f0
AgustinSaguier <Agustinsaguier420@gmail.com>
2022-04-21 14:24:36 -0400

    name change

--- Informatica 3A/formulario_14/divisiones.php
+++ Informatica 3A/formulario_14/divisiones.php
@@ -2,17 +2,23 @@
 $dividendo=$_REQUEST["dividendo"];
 $divisor=$_REQUEST["divisor"];
 
-$cociente=$dividendo/$divisor;
-$resto=$dividendo%$divisor;
-
-if (is_float($cociente)){
-    echo "<p>La division no es exacta<br>
-    Cociente: $cociente<br>
-    Resto: $resto</p>";
+if ($divisor==0){
+    echo "ERROR: Divison por cero";
 }
 else {
-    echo "<p>La division es exacta<br>
-    Cociente: $cociente<br>
-    Resto: $resto</p>";
-}
-?>
+    $cociente=$dividendo/$divisor;
+    $realCociente=number_format($cociente);
+    $resto=$dividendo%$divisor;
+
+    if (is_float($cociente)){
+        echo "<p>La division no es exacta<br>
+        Cociente: $realCociente<br>
+        Resto: $resto</p>";
+    }
+    else {
+        echo "<p>La division es exacta<br>
+        Cociente: $realCociente<br>
+        Resto: $resto</p>";
+    }
+ }
+    ?>
