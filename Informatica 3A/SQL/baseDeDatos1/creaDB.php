df8ff751d214a9c55311b93ddc4381759fdbb95b
AgustinSaguier <Agustinsaguier420@gmail.com>
2022-04-26 15:44:33 -0400

    name change

---
+++ Informatica 3A/SQL/baseDeDatos1/creaDB.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Crear un archivo creaDB.php que tenga el
+comando como texto sql para cerar
+una base de datos que se llame colegio
+*/
+
+    $sql="CREATE DATABASE colegio";
+    
+
+
+
+?>
