080d33465ea46bbff8950514f9e0cc197c0a5120
AgustinSaguier <Agustinsaguier420@gmail.com>
2022-04-29 11:40:18 -0400

    name change

--- Informatica 3A/SQL/baseDeDatos1/creaDB.php
+++ Informatica 3A/SQL/baseDeDatos1/creaDB.php
@@ -1,13 +1,15 @@
+
 <?php
 /*
 Crear un archivo creaDB.php que tenga el
-comando como texto sql para cerar
+comando como texto sql para crear
 una base de datos que se llame colegio
 */
 
     $sql="CREATE DATABASE colegio";
-    
-
-
-
+    //Primer Paso: Conectar al servidor
+    $connect=mysqli_connect("localhost","agustin","12345678") or die ("ERROR DE CONEXION AL SERVIDOR!!");
+    echo ("Conexion al servidor exitosa");
+    mysqli_query($connect,$sql) or die ("<br>ERROR EN EJECUTAR COMANDO SQL");
+    echo ("<br>Base de datos creada exitosamente");
 ?>
---
+++ Informatica 3A/SQL/baseDeDatos1/creatabla.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $sql="CREATE TABLE  Alumnos (
+        CI int NOT NULL,
+        Name varchar(255) NOT NULL,
+        LastName varchar(255),
+        Age int,
+        PRIMARY KEY (CI)
+    );"
+
+
+
+    ?>
+</body>
+</html>
