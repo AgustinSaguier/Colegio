f19f364d32fe021a6838c940f5469d2e75dfa7b1
AgustinSaguier <Agustinsaguier420@gmail.com>
2022-05-05 14:26:03 -0400

    name change

--- Informatica 3A/SQL/baseDeDatos2/procesaTabla.php
+++ Informatica 3A/SQL/baseDeDatos2/procesaTabla.php
@@ -8,7 +8,19 @@
 </head>
 <body>
     <?php
-    include 
+    include 'conexion.php';
+    /*recuperar los datos que vamos a incluir
+    en la base de datos*/
+    $idLibro=$_REQUEST["idLibro"];
+    $nacionalidad=$_REQUEST["nacionalidad"];
+    $año=$_REQUEST["año"];
+    //seleccionar la base de datos
+    mysqli_select_db($link,"libreria") or die ("<br>ERROR AL SELECCIONAR LA BASE DE DATOS");
+    echo ("<br>Se selecciono la base de datos con exito");
+    //el sql que permitira introducir los datos
+    $sql="insert into libros (idLibro,Nacionalidad,Año) values ($idLibro,$nacionalidad,$año)";
+    
+    
     
     ?>
 </body>
