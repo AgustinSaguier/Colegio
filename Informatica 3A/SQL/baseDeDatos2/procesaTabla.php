a5cf5762c353c07aee6932117a571bb82e4af3ee
AgustinSaguier <Agustinsaguier420@gmail.com>
2022-05-06 12:37:19 -0400

    name change

--- Informatica 3A/SQL/baseDeDatos2/procesaTabla.php
+++ Informatica 3A/SQL/baseDeDatos2/procesaTabla.php
@@ -33,13 +33,13 @@
         //en la base de datos
         $idAutor=$_REQUEST["idAutor"];
         $nacionalidad=$_REQUEST["nacionalidad"];
-        $añoNacimeinto=$_REQUEST["añoNacimiento"];
-        $nombre=$_REQUEST["nombre"]
+        $añoNacimiento=$_REQUEST["añoNacimiento"];
+        $nombre=$_REQUEST["nombre"];
         //seleccionar la base de datos
         mysqli_select_db($link,"libreria") or die ("<br>ERROR AL SELECCIONAR LA BASE DE DATOS");
         echo ("<br>Se selecciono la base de datos con exito");
         //el sql que permitira introducir los datos
-        $sql="INSERT INTO libros (idAutor, Nacionalidad, Nombre, AñoNacimiento) VALUES ('$idAutor','$nacionalidad','$nombre','$añoNacimiento')";
+        $sql="INSERT INTO autor (idAutor, Nacionalidad, Nombre, AñoNacimiento) VALUES ('$idAutor','$nacionalidad','$nombre','$añoNacimiento')";
         //ejecutar el comando sql
         mysqli_query ($link,$sql) or die ("<br>ERROR AL INSERTAR DATOS");
         echo ("<h3>Se cargaron los datos a la tabla AUTORES</h3>");
@@ -53,17 +53,17 @@
         $idEditorial=$_REQUEST["idEditorial"];
         $email=$_REQUEST["email"];
         $direccion=$_REQUEST["direccion"];
-        $nombre=$_REQUEST["nombre"]
+        $nombre=$_REQUEST["nombre"];
         //seleccionar la base de datos
         mysqli_select_db($link,"libreria") or die ("<br>ERROR AL SELECCIONAR LA BASE DE DATOS");
         echo ("<br>Se selecciono la base de datos con exito");
         //el sql que permitira introducir los datos
-        $sql="INSERT INTO libros (idEditorial, Email, Nombre, Direccion) VALUES ('$idEditorial','$email','$nombre','$direccion')";
+        $sql="INSERT INTO editorial (idEditorial, Email, Nombre, Direccion) VALUES ('$idEditorial','$email','$nombre','$direccion')";
         //ejecutar el comando sql
         mysqli_query ($link,$sql) or die ("<br>ERROR AL INSERTAR DATOS");
         echo ("<h3>Se cargaron los datos a la tabla EDITORIAL</h3>");
         mysqli_close ($link);
-        echo ("<a href='cargaTabla.html'>Cargar Otro</a>")
+        echo ("<a href='cargaTabla.html'>Cargar Otro</a>");
     }
     ?>
 </body>
