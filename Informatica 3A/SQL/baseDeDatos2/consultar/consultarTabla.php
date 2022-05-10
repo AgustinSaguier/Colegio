af032fe002eef7c55f9be42e2a2d9bb2143e7c3e
AgustinSaguier <Agustinsaguier420@gmail.com>
2022-05-10 15:45:17 -0400

    name change

--- Informatica 3A/SQL/baseDeDatos2/consultar/consultarTabla.php
+++ Informatica 3A/SQL/baseDeDatos2/consultar/consultarTabla.php
@@ -23,17 +23,37 @@
         //cuento la cantidad de filas
         $cantFilas=mysqli_num_rows($tabla);
         echo("<hr>");
-        for ($k=0;$k<$cantFilas;$k++){
+        /*for ($k=0;$k<$cantFilas;$k++){
             //Recupero la fila
             $fila=mysqli_fetch_array($tabla);
             echo ("idLibro: ".$fila["idLibro"]);
             echo ("<br>");
             echo ("Año: ".$fila["Año"]);
             echo ("<br>");
-            echo ("Nacionalidad".$fila["Nacionalidad"]);
+            echo ("Nacionalidad: ".$fila["Nacionalidad"]);
             echo ("<br>");
             echo ("<hr>");
         }
+        */
+        echo("<table>");
+        for ($z=0;$z<=$cantFilas;$z++){ 
+        if ($z==0){
+            echo("<tr>
+            <td>idLibro</td>
+            <td>Año</td>
+            <td>Nacionalidad</td>
+            </tr>");
+        }
+        else{
+            $fila=mysqli_fetch_array($tabla);
+            echo("<tr>
+            <td>".$fila["idLibro"]."</td>
+            <td>".$fila["Año"]."</td>
+            <td>".$fila["Nacionalidad"]."</td>
+            </tr>");
+        }
+        }
+        echo("</table>");
         mysqli_close($link);
     }
     else if ($code==2){
@@ -58,6 +78,35 @@
             echo ("Nacionalidad: ".$fila["Nacionalidad"]);
             echo ("<br>");
             echo ("Año de Nacimiento: ".$fila["AñoNacimiento"]);
+            echo ("<br>");
+            echo ("Nombre: ".$fila["Nombre"]);
+            echo ("<br>");
+            echo ("<hr>");
+        }
+        mysqli_close($link);
+    }
+    else if ($code==3){
+        include "../conexion.php";
+        mysqli_select_db ($link, "libreria") or die ("No encuentro la base de datos");
+        //comando SQL
+        $sql="SELECT * FROM editorial";
+        //ejecutar el comando SQL
+        //OBS: como este ees comando trae info necesitamos 
+        //igualar a una variable
+        $tabla=mysqli_query($link,$sql) or die ("Error en la consulta");
+        //logica de negocios
+        //ver si recupero alguna informacion
+        //cuento la cantidad de filas
+        $cantFilas=mysqli_num_rows($tabla);
+        echo("<hr>");
+        for ($k=0;$k<$cantFilas;$k++){
+            //Recupero la fila
+            $fila=mysqli_fetch_array($tabla);
+            echo ("idEditorial: ".$fila["idEditorial"]);
+            echo ("<br>");
+            echo ("Email: ".$fila["Email"]);
+            echo ("<br>");
+            echo ("Direccion: ".$fila["Direccion"]);
             echo ("<br>");
             echo ("Nombre: ".$fila["Nombre"]);
             echo ("<br>");
