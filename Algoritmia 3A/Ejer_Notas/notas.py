cce0f1547f192ecac0a10e3a52c53a9f1ee066f5
Agustin Saguier <Agustinsaguier420@gmail.com>
2022-05-12 20:55:52 -0400

    name change

--- Algoritmia 3A/Ejer_Notas/notas.py
+++ Algoritmia 3A/Ejer_Notas/notas.py
@@ -4,7 +4,7 @@
 #Escribir un programa que permita obtener el promedio de
 #calificacion de cada alumno.
 #El promedio debera ser almacenado en otro archivo .txt
-
+prom=0
 with open("notas.txt") as file, open ("promedio.txt","w") as promedio:
     for record in file:
         record=record.strip()
@@ -13,5 +13,15 @@ with open("notas.txt") as file, open ("promedio.txt","w") as promedio:
             if values[0]!= "Apellidos":
                 promedio.write("----------Alumno/a: "+values[0]+"--------------")
                 promedio.write('\n')
-                promedio.write('Su promedio es de '+str((int(values[1])+int(values[2])+int(values[3])+int(values[4])+int(values[5])+int(values[6]))/6))
+                prom=0
+                for k in range (len(values)):
+                    if (k!=0):
+                        prom=prom+int(values[k])
+                num=len(values)-1
+                newProm=prom/num
+                promedio.write('Su promedio es de '+str(newProm))
                 promedio.write('\n')
+                
+                   
+
+
