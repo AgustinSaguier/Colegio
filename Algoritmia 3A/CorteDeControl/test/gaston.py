92f27892ccfd7c16e33ea939eead4bec34cd65c4
Agustin Saguier <Agustinsaguier420@gmail.com>
2022-05-12 22:18:39 -0400

    name change

--- Algoritmia 3A/CorteDeControl/test/gaston.py
+++ Algoritmia 3A/CorteDeControl/test/gaston.py
@@ -1,48 +1,51 @@
-sumatotal=0
+
 contador=0
+sumaTotal=0
 promedio=0
 lista=[]
-
-with open ("gaston.txt","r") as archivo, open ("gastonEditado.txt","w") as editado:
-    linea=archivo.readline()
-    linea=linea.strip()
-    lista.append(linea)
-    for linea in archivo:
-        linea=linea.strip()
-        lista.append(linea)
-        nuevaLista=sorted(lista)
-    for dato in nuevaLista:
-        editado.write(dato)
-        editado.write("\n")
+with open("gaston.txt","r") as archivo, open("gastonNew.txt","w") as new:
+    line=archivo.readline()
+    line=line.strip()
+    lista.append(line)
+    for line in archivo:
+        line=line.strip()
+        lista.append(line)
+    print (lista)
+    sortedLista=sorted(lista)
+    print(sortedLista)
+    for item in sortedLista:
+        new.write(item+"\n")
+        
+    
 try:
-    with open ("gastonEditado.txt","r") as file, open ("resumen.txt","w") as resumen:
+    with open ("gastonNew.txt","r") as file, open ("resumen.txt","w") as resumen:
         linea=file.readline()
         linea=linea.strip()
-        separado=linea.split(",")
-        claveActual=separado[0]
-        valor=int(separado[1])
+        lineaSpliteada=linea.split(",")
+        claveActual=lineaSpliteada[0]
+        suma=int(lineaSpliteada[1])
         claveAnterior=claveActual
         while (linea!=""):
             if (claveActual==claveAnterior):
-                sumatotal=sumatotal+valor
+                sumaTotal=sumaTotal+suma
                 contador+=1
                 linea=file.readline()
                 linea=linea.strip()
                 if (linea==""):
                     break
-                separado=linea.split(",")
-                claveActual=separado[0]
-                valor=int(separado[1])
+                lineaSpliteada=linea.split(",")
+                claveActual=lineaSpliteada[0]
+                suma=int(lineaSpliteada[1])
             else:
-                promedio=sumatotal/contador
-                print ("La clave "+claveAnterior+" tiene una total de "+str(sumatotal)+", tiene un promedio de "+str(promedio)+ " y aparece una cantidad de " +str(contador) + " veces")
-                resumen.write("La clave "+claveAnterior+" tiene una total de "+str(sumatotal)+", tiene un promedio de "+str(promedio)+ " y aparece una cantidad de " +str(contador) + " veces\n")
+                promedio=sumaTotal/contador
+                print("La clave " + claveAnterior + " tiene una suma total de " + str(sumaTotal) +", tiene un promedio de "+ str(promedio) + " y aparece una cantidad de " + str(contador) + " veces")
+                resumen.write("La clave " + claveAnterior + " tiene una suma total de " + str(sumaTotal) +", tiene un promedio de "+ str(promedio) + " y aparece una cantidad de " + str(contador) + " veces\n")
                 claveAnterior=claveActual
-                sumatotal=0
+                sumaTotal=0
                 contador=0
                 promedio=0
-        promedio=sumatotal/contador
-        print ("La clave "+claveAnterior+" tiene una total de "+str(sumatotal)+", tiene un promedio de "+str(promedio)+ " y aparece una cantidad de " +str(contador) + " veces")
-        resumen.write("La clave "+claveAnterior+" tiene una total de "+str(sumatotal)+", tiene un promedio de "+str(promedio)+ " y aparece una cantidad de " +str(contador) + " veces")
+        promedio=sumaTotal/contador
+        print("La clave " + claveAnterior + " tiene una suma total de " + str(sumaTotal) +", tiene un promedio de "+ str(promedio) + " y aparece una cantidad de " + str(contador) + " veces")
+        resumen.write("La clave " + claveAnterior + " tiene una suma total de " + str(sumaTotal) +", tiene un promedio de "+ str(promedio) + " y aparece una cantidad de " + str(contador) + " veces")
 except IOError:
     print("ERROR")
