bb1d6dd12c2d9c0d0882f1cbb743bbe187bae97a
Agustin Saguier <Agustinsaguier420@gmail.com>
2022-07-31 19:35:44 -0400

    name change

---
+++ Informatica 3A/SQL/baseDeDatos8/db.php
@@ -0,0 +1,33 @@
+<?php
+    $sql="CREATE DATABASE Examen";
+
+    $connect=mysqli_connect("localhost","agustin","12345678") or die ("ERROR DE CONEXION AL SERVIDOR!!");
+    echo ("Conexion al servidor exitosa");
+    mysqli_query($connect,$sql) or die ("<br>ERROR EN EJECUTAR COMANDO SQL");
+    echo ("<br>Base de datos creada exitosamente");
+    $tablaUno="CREATE TABLE Uno (
+        id int AUTO_INCREMENT,
+        Username varchar(20) NOT NULL,
+        Password varchar(20) NOT NULL,
+        PRIMARY KEY (id)
+    );";
+    $tablaDos="CREATE TABLE Dos (
+        id int AUTO_INCREMENT,
+        Username varchar(20) NOT NULL,
+        Password varchar(20) NOT NULL,
+        PRIMARY KEY (id)
+    );";
+
+
+    $link=mysqli_connect("localhost","agustin","12345678") or die ("ERROR DE CONEXION AL SERVIDOR!!");
+    echo ("Conexion al servidor exitosa");
+    //ESTA FUNCION SIRVE PARA SELECCIONAR LA BASE DE DATOS
+    mysqli_select_db($link,"Examen") or die ("<br>ERROR AL SELECCIONAR LA BASE DE DATOS");
+    echo ("<br>Se selecciono la base de datos con exito");
+    mysqli_query($link,$tablaUno) or die ("<br>ERROR AL EJECUTAR COMANDO SQL 1");
+    echo("<br>Se ha creado la tabla");
+    mysqli_query($link,$tablaDos) or die ("<br>ERROR AL EJECUTAR COMANDO SQL 1");
+    echo("<br>Se ha creado la tabla");
+    mysqli_close($link) or die ("<br>ERROR Al CERRAR BASE DE DATOS");
+    echo("<br>Se cerro el la base de datos con exito");
+?>
--- Informatica 3A/SQL/baseDeDatos8/includes/conexion.php
+++ Informatica 3A/SQL/baseDeDatos8/includes/conexion.php
@@ -1,7 +1,7 @@
 <?php
 $user='agustin';
 $pass='12345678';
-$db='';
+$db='Examen';
 $servidor='localhost';
 $link=mysqli_connect($servidor,$user,$pass)
 or die ("ERROR DE CONEXION AL SERVIDOR!!");
