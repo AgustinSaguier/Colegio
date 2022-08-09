823cd7d10fb1ce75a8d06e4439b1fa7592afe90f
Agustin Saguier <Agustinsaguier420@gmail.com>
2022-08-08 21:58:28 -0400

    name change

---
+++ INTERCOLEGIAL 3A/includes/conexion.php
@@ -0,0 +1,9 @@
+<?php
+$user='root';
+$pass='';
+$db='intercolegial';
+$servidor='localhost';
+$link=mysqli_connect($servidor,$user,$pass)
+or die ("ERROR DE CONEXION AL SERVIDOR!!");
+mysqli_select_db($link,$db);
+?>
