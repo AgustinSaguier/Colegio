695ac9de137b9da873bb5bc580d0b118380512d9
AgustinSaguier <Agustinsaguier420@gmail.com>
2022-10-18 15:40:39 -0300

    name change

--- Informatica 3A/JS/eventos.js
+++ Informatica 3A/JS/eventos.js
@@ -1,2 +1,11 @@
 let img = document.getElementById("img")
 let container = document.getElementById("container")
+
+img.ondragstart = function () {
+    console.log("Hello World")
+}
+
+img.addEventListener("dragstart",startMovement)
+function startMovement (){
+    console.log("Movement detected")
+}
